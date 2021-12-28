7a21a10cc25f7c353f9a0cb1cf28e8029810aaf9
sskurochkin <semenkurochkin@gmail.com>
2021-12-28 21:31:48 +0300

    redux with js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,69 @@
-import React from 'react';
-
+import React from "react";
+import { createStore } from "redux";
 
 function App() {
-    return ( <div>
-        <button className='btn btn-warning'>Success</button>
-    </div> );
+
+    const initialState = {value : 0}
+
+	const reducer = (state = initialState, action) => {
+		switch (action.type) {
+			case "INC":
+				return {
+                    ...state,
+                    value: state.value + 1
+                };
+			case "DEC":
+				return {
+                    ...state,
+                    value: state.value - 1
+                };
+			case "RND":
+				return {
+                    ...state,
+                    value: state.value * action.payload
+                };
+
+			default:
+				return state;
+		}
+	};
+
+	const store = createStore(reducer);
+	const update = () =>
+		(document.getElementById("counter").textContent = store.getState().value);
+
+	store.subscribe(update);
+
+	const inc = () => ({type: "INC"});
+	const dec = () => ({type: "DEC"});
+	const rnd = (value) => ({type: "RND", payload: value});
+
+
+	document.getElementById("inc").addEventListener("click", () => {
+		store.dispatch(inc());
+	});
+	document.getElementById("dec").addEventListener("click", () => {
+		store.dispatch(dec());
+	});
+	document.getElementById("rnd").addEventListener("click", () => {
+		const value = Math.floor(Math.random() * 10);
+		store.dispatch(rnd(value));
+	});
+
+	// console.log(store.getState());
+
+	// let state = reducer(initialState, { type: "INC" });
+	// state = reducer(state, { type: "INC" });
+	// state = reducer(state, { type: "INC" });
+
+	// console.log(state)
+
+	return (
+		<div>
+			<button className='btn btn-warning'>INC</button>
+			<button className='btn btn-danger'>DECR</button>
+		</div>
+	);
 }
 
 export default App;

d21bb0e4932bb30f226fad8a9d0785c48efc9e28
sskurochkin <semenkurochkin@gmail.com>
2022-01-05 19:50:17 +0300

    add hooks

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,24 +1,33 @@
-import { connect } from "react-redux";
-import * as actions from '../actions'
+import { connect, useSelector, useDispatch } from "react-redux";
+// import * as actions from '../actions'
+import {inc, dec, rnd} from '../actions'
 // import { bindActionCreators } from "redux";
 
 
-const Counter = ({counter, inc, dec, rnd}) => {
+const Counter = () => {
+
+    const counter = useSelector(state => state.counter)
+    const dispatch = useDispatch();
+
+
+
+
+
     return (
         <div className="jumbotron">
             <h1>{counter}</h1>
-            <button onClick={dec} className="btn btn-primary">DEC</button>
-            <button onClick={inc} className="btn btn-primary">INC</button>
-            <button onClick={rnd} className="btn btn-primary">RND</button>
+            <button onClick={()=> dispatch(dec())} className="btn btn-primary">DEC</button>
+            <button onClick={()=> dispatch(inc())} className="btn btn-primary">INC</button>
+            <button onClick={()=> dispatch(rnd())} className="btn btn-primary">RND</button>
         </div>
     )
 }
 
-const mapStateToProps = (state) =>{ // чистая и синхронная, возвращает объект со стейтом
-	return {
-		counter: state.value
-	}
-}
+// const mapStateToProps = (state) =>{ // чистая и синхронная, возвращает объект со стейтом
+// 	return {
+// 		counter: state.value
+// 	}
+// }
 
 // const mapDispatchToProps = (dispatch) => {
 // 	return bindActionCreators(actions, dispatch)
@@ -35,5 +44,6 @@ const mapStateToProps = (state) =>{ // чистая и синхронная, в�
 // 	}
 // }
 
-export default connect(mapStateToProps, actions)(Counter);
+// export default connect(mapStateToProps, actions)(Counter);
 // export default connect(mapStateToProps, mapDispatchToProps)(Counter);
+export default Counter
--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ import { createStore } from "redux";
 import { reducer } from "./reducer";
 import { Provider } from "react-redux";
 
-const store = createStore(reducer)
+const store = createStore(reducer, 
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 ReactDOM.render(
 	<React.StrictMode>
